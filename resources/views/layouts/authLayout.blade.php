c6dc746d8af4e4769ccf20266de7e693a8748ba3
sheikdaw <52328052+sheikdaw@users.noreply.github.com>
2026-04-29 14:17:21 +0530

    main

--- resources/views/layouts/authLayout.blade.php
+++ resources/views/layouts/authLayout.blade.php
@@ -50,7 +50,7 @@
             width: 100%;
             height: 100%;
             object-fit: cover;
-            brightness: 40%;
+            brightness: 80%;
             transform: scale(1.02);
             animation: slowZoom 22s ease infinite alternate;
         }
