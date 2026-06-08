95437d4b27b20e079825b160fb7efa9214b0f19b
coimbatoresurvey62-blip <coimbatoresurvey62@gmail.com>
2026-06-08 09:15:45 +0530

    main

--- resources/views/surveyor/mapview.blade.php
+++ resources/views/surveyor/mapview.blade.php
@@ -2366,7 +2366,7 @@
                         },
                         success: function(response) {
                             polygons = response.polygons;
-                             points = response.points;
+                            points = response.points;
                             refreshVectorLayer();
                             showFlashMessage(response.message, "success");
                             $("#editToolSelect").val("none");
